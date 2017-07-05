844b81445cc766df2c7badc36818cd72405b4a26
Henrik Melsom <melsom.henrik@gmail.com>
2017-07-05 22:53:25 +0200

    added addon barebones

--- __init__.py
+++ __init__.py
@@ -9,5 +9,30 @@ bl_info = {
     "description": "Arctic Beard Studio in-house toolkit, because you're worth it!",
     "warning": "",
     "wiki_url": "",
-    "category": ""
+    "category": "System"
 }
+
+import bpy
+
+
+class Align(bpy.types.Operator):
+    """I heard you like aligning stuff, so i wrote this function so you can align stuff to other stuff."""
+    bl_idname = "arctic_beard_studio.align"
+    bl_label = "Align"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        print("Shit just got aligned! *harkle*... actually this is just a dummy function... #sadface :(")
+        return {'FINISHED'}
+
+
+def register():
+    bpy.utils.register_class(Align)
+
+
+def unregister():
+    bpy.utils.unregister_class(Align)
+
+
+if __name__ == "__main__":
+    register()
