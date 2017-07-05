7d1e3c14293aa2186d7faeabadd3f56a46ad15d5
Henrik Melsom <melsom.henrik@gmail.com>
2017-07-05 21:29:51 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# Copyright Arctic Beard Studio 2017
+
+bl_info = {
+    "name": "Arctic Beard Studio Toolkit",
+    "author": "Henrik Melsom",
+    "version": (0, 1),
+    "blender": (2, 76, 0),
+    "location": "",
+    "description": "Arctic Beard Studio in-house toolkit, because you're worth it!",
+    "warning": "",
+    "wiki_url": "",
+    "category": ""
+}
